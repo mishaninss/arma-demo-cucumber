5e42f02a66855baafe2ac9936c29913318b25729
Sergey_Mishanin <sergey.mishanin@nytimes.com>
2018-03-21 15:55:25 +0300

    upd

--- src/test/java/com/github/myproject/RunDebugIT.java
+++ src/test/java/com/github/myproject/RunDebugIT.java
@@ -1,4 +1,20 @@
-package com.github.mishaninss;
+/*
+ * Copyright 2018 Sergey Mishanin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.myproject;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
@@ -11,7 +27,7 @@ import org.slf4j.LoggerFactory;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/steps",
-        glue = "com.github.mishaninss.steps",
+        glue = "com.github.myproject.steps",
         strict = true,
         plugin = {
 
